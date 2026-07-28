93222236ecca9a3f4a9395f7e8bb0b3c72a10d77
MỘC DU NHIÊN - TRẠM TÁM 247 <nguyenye742@gmail.com>
2026-07-28 18:21:02 +0700

    Update data.js

--- data.js
+++ data.js
@@ -4,13 +4,13 @@ const resourcesData = [
         category: "khoa-hoc",
         title: "Đề Ôn Tập Cuối Học Kỳ 1 - Vật Lý 10 (Đề Số 1)",
         desc: "Đề thi kèm lời giải chi tiết (Thầy Lại Đắc Hợp - Moon.vn): Đầy đủ Trắc nghiệm, Đúng/Sai và Tự luận nâng cao.",
-        link: "DÁN_LINK_GOOGLE_DRIVE_ĐỀ_1_VÀO_ĐÂY"
+        link: "https://drive.google.com/file/d/1GmfBcZdaSFi9PkVWLnEE933ivwY5OkOs/view?usp=sharing"
     },
     {
         tagName: "Vật Lý 10",
         category: "khoa-hoc",
         title: "Đề Ôn Tập Cuối Học Kỳ 1 - Vật Lý 10 (Đề Số 2)",
         desc: "Bộ đề ôn tập trắc nghiệm & tự luận HK1 kèm lời giải chi tiết (Thầy Lại Đắc Hợp - Moon.vn).",
-        link: "DÁN_LINK_GOOGLE_DRIVE_ĐỀ_2_VÀO_ĐÂY"
+        link: "https://drive.google.com/file/d/1jPcTrVIUK_PaGMVyXu7FMGdvsks5vNV9/view?usp=sharing"
     }
 ];
