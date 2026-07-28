b5099aaaad93eb6f3e159666cf35898b9263fd9e
MỘC DU NHIÊN - TRẠM TÁM 247 <nguyenye742@gmail.com>
2026-07-28 19:00:30 +0700

    Update data.js

--- data.js
+++ data.js
@@ -12,5 +12,12 @@ const resourcesData = [
         title: "Đề Ôn Tập Cuối Học Kỳ 1 - Vật Lý 10 (Đề Số 2)",
         desc: "Bộ đề ôn tập trắc nghiệm & tự luận HK1 kèm lời giải chi tiết (Thầy Lại Đắc Hợp - Moon.vn).",
         link: "https://drive.google.com/file/d/1jPcTrVIUK_PaGMVyXu7FMGdvsks5vNV9/view?usp=sharing"
+    },
+    {
+        tagName: "Video YouTube",
+        category: "khoa-hoc",
+        title: "🎬 [Video] VẬT LÝ 10 - Bài 1: Làm Quen Với Vật Lí",
+        desc: "Video bài giảng chi tiết Bài 1: Làm quen với Vật lí (Vật lý 10). Tải tài liệu đính kèm trên website.",
+        link: "https://youtu.be/hzhv-_aaVU0"
     }
 ];
