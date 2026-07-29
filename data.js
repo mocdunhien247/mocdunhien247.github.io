99f54c4f47a36a20c4bfed76b2dd6444121c3af1
MỘC DU NHIÊN - TRẠM TÁM 247 <nguyenye742@gmail.com>
2026-07-30 04:52:03 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,21 +1,26 @@
+// Khai báo mảng chứa dữ liệu
 let resourcesData = [];
 
 // Tải dữ liệu từ file resources.json
 fetch('./resources.json')
   .then(response => {
     if (!response.ok) {
-      throw new Error("Không thể tải file resources.json");
+      throw new Error("Lỗi khi tải file resources.json");
     }
     return response.json();
   })
   .then(data => {
     resourcesData = data;
-    
-    // Gọi lại hàm vẽ giao diện web của bạn (ví dụ: renderResources() hoặc displayData())
-    if (typeof renderResources === 'function') {
-      renderResources();
+
+    // Tự động tìm và gọi hàm hiển thị danh sách trên trang web của bạn
+    if (typeof renderCards === 'function') {
+      renderCards(resourcesData);
+    } else if (typeof renderResources === 'function') {
+      renderResources(resourcesData);
+    } else if (typeof displayResources === 'function') {
+      displayResources(resourcesData);
+    } else if (typeof renderData === 'function') {
+      renderData(resourcesData);
     }
   })
-  .catch(error => {
-    console.error("Lỗi tải dữ liệu:", error);
-  });
+  .catch(error => console.error("Lỗi tải dữ liệu:", error));
